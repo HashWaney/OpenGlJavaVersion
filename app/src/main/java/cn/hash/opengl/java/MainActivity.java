bf5abcba997ce38ffa7c00c28ce776535880976d
王庆 <wangqing@wangqingdeMacBook-Pro.local>
2020-04-08 21:45:52 +0800

    update readme

--- app/src/main/java/cn/hash/opengl/java/MainActivity.java
+++ app/src/main/java/cn/hash/opengl/java/MainActivity.java
@@ -1,14 +1,64 @@
- package cn.hash.opengl.java;
+package cn.hash.opengl.java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected GLSurfaceView myGlSurfaceView;
+    protected boolean rendererSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //1。 init GLSurfaceView instance
+
+        myGlSurfaceView = new GLSurfaceView(this);
+
+
+        //1.2 check for support Es2
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        boolean supportEs2 = configurationInfo.reqGlEsVersion >= 0x2000;
+
+        if (!supportEs2) {
+            Toast.makeText(this,
+                    "This device is not support Opengl es2 ", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        myGlSurfaceView.setEGLContextClientVersion(2);
+        myGlSurfaceView.setRenderer(new OpenGlRenderer());
+        rendererSet = true;
+
+        //2. set GlSurfaceView  to ContentView
+        setContentView(myGlSurfaceView);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //to order activity lifecycle  so can avoid crash
+        if (myGlSurfaceView != null) {
+            myGlSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (myGlSurfaceView!=null)
+        {
+            myGlSurfaceView.onResume();;
+        }
     }
 }
---
+++ app/src/main/java/cn/hash/opengl/java/OpenGlRenderer.java
@@ -0,0 +1,51 @@
+package cn.hash.opengl.java;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glViewport;
+
+/**
+ * Created by Hash on 2020-04-08.
+ */
+
+
+public class OpenGlRenderer implements GLSurfaceView.Renderer {
+
+
+    //when surface create invoke this function, maybe device wakeup or
+    // switch back form another activity this function also be invoked.
+    // so this function can invoke at least once
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        glClearColor(1.0f, 0f, 0f, 0f);
+
+    }
+
+    // when surface change size , vertical or horizontal switch surface size
+    // change so this function invoke
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        //set the opengl viewport to fill the entire surface
+        glViewport(0, 0, width, height);
+        Log.i("Hash", "width: " + width + " height:" + height);
+
+
+    }
+
+    //when draw per frame this function invoke,
+    //also we should draw some thing, even if clear screen
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        //clear the  rendering surface  and invoke the glClearColor to fill the screen
+        glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+    }
+}
